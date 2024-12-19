909efd49c02bcce8314be6f8c7e35d5ea6458ed5
BuLaY01 <mishastar544@gmail.com>
2024-12-19 21:01:39 +0200

    finderAccountall

--- gitpage/script.js
+++ gitpage/script.js
@@ -1,14 +1,60 @@
-let key = document.getElementById('key')
-let search = document.getElementById('search')
-let card = document.getElementById('card')
-let setting = document.getElementById('setting')
+let key = document.getElementById("key");
+let search = document.getElementById("search");
+let card = document.getElementById("card");
+let setting = document.getElementById("setting");
 
-let saver ;
+let saver;
 
-search.addEventListener('click', result)
+search.addEventListener("click", result);
 
-function result() {
-    setting.classList.remove('hider')
-    search.classList.add('hider')
-    key.classList.add('hider')
+async function result() {
+  setting.classList.remove("hider");
+  search.classList.add("hider");
+  key.classList.add("hider");
+  card.classList.add('hider')
+
+  let responce = await fetch(`https://api.github.com/users/${key.value}`);
+
+  key.value = ''
+
+  if (responce.ok) {
+    saver = await responce.json();
+    console.log(saver.login);
+    card_gen();
+  } else {
+    console.log("Данного пользователя не существует.");
+    setting.classList.add("hider");
+    search.classList.remove("hider");
+    key.classList.remove("hider");
+  }
+}
+
+function card_gen() {
+  card.innerHTML = `
+        <img
+        src="${saver.avatar_url}"
+      />
+      <h1>${saver.login}</h1>
+      <p>${saver.bio}</p>
+      <nav>
+        <div>
+          <i class="fas fa-map-marker-alt"></i>
+          <span>${saver.location}</span>
+        </div>
+        <div>
+          <i class="fas fa-heart-circle"></i>
+          <span>${saver.followers}</span>
+        </div>
+        <div>
+          <i class="fas fa-book"></i>
+          <span>${saver.public_repos}</span>
+        </div>
+      </nav>
+      <a href="${saver.html_url}" target = "_blank">Перейти</a>
+    `;
+
+  card.classList.remove("hider");
+  setting.classList.add("hider");
+  search.classList.remove("hider");
+  key.classList.remove("hider");
 }
